5d4fd5dcbd1f5242aea2817b26750e5c6abe4844
leonardo silva <silvaleonardo.ti@gmail.com>
2021-01-29 19:33:36 -0300

    Alterações nos componentes de TodoList e TodoListItem

--- src/components/TodoListItem.tsx
+++ src/components/TodoListItem.tsx
@@ -6,16 +6,28 @@ interface Itodo{
 }
 
 const TodoListItem = (props: Itodo) =>{
+
+    const handleChange = () => {
+        console.log('change status')
+    }
+
+    const onRemove = (Todo: Todo) => {
+        console.log(Todo)
+    }
+
     return(
         <tr className="uk-animation-slide-bottom-medium">
             <td className="uk-width-auto">
                 <label>
-                    <input className="uk-checkbox" type="checkbox" />
+                    <input className="uk-checkbox" type="checkbox" 
+                    checked={props.todo.done}
+                    onChange={handleChange}/>
                 </label>
             </td>
             <td className="uk-width-expand">{props.todo.title}</td>
             <td className="uk-width-auto">
-                <button className="uk-icon-button uk-button-danger" uk-icon="trash"></button>
+                <button className="uk-icon-button uk-button-danger" uk-icon="trash"
+                onClick={() => onRemove(props.todo)}></button>
             </td>
         </tr>
     )
